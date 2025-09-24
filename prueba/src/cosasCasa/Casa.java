4a20341467c25ba9bb532bc18cdc6aa35522ef08
vricheb973 <vricheb973@g.educaand.es>
2025-09-24 14:35:15 +0200

    Añadidas tareas de clase

--- prueba/src/cosasCasa/Casa.java
+++ prueba/src/cosasCasa/Casa.java
@@ -5,6 +5,8 @@ public class Casa {
 	public static void main(String[] args) {
 
 		System.out.println("Me he hartado de trabajar en casa");
+		
+		System.out.println("Sigo con la tarea de casa");
 
 	}
 
