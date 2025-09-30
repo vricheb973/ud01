024d676726e2367a83cc9e8b466af4d25f840b90
vricheb973 <vricheb973@g.educaand.es>
2025-09-30 14:08:22 +0200

    Añadidos ejercicios de ejemplo

---
+++ prueba/src/ejercicios/Diapositiva77.java
@@ -0,0 +1,29 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Diapositiva77 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime las peras que vendiste el primer semestre: ");
+		double peras1 = sc.nextDouble();
+		
+		System.out.println("\nDime las peras que vendiste el segundo semestre: ");
+		double peras2 = sc.nextDouble();
+		
+		System.out.println("\nDime las manzanas que vendiste el primer semestre: ");
+		double manzanas1 = sc.nextDouble();
+		
+		System.out.println("\nDime las manzanas que vendiste el segundo semestre: ");
+		double manzanas2 = sc.nextDouble();
+		
+		double total = (peras1+peras2)*1.95 + (manzanas1+manzanas2)*2.35;
+		
+		System.out.printf("\nEl importe total es %.2f € \n", total);
+
+	}
+
+}
