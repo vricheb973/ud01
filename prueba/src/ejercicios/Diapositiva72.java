28b05ca346ad42093a4f0eae32157f14a22a1553
vricheb973 <vricheb973@g.educaand.es>
2025-09-30 12:46:14 +0200

    Añadidos ejercicios

---
+++ prueba/src/ejercicios/Diapositiva72.java
@@ -0,0 +1,27 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Diapositiva72 {
+	
+	public static void main(String[] args) {
+		
+		// si dividimos un número entre 2
+		/*
+		 *  el resto puede ser:
+		 *  	- 0 (indica que la división es exacta)
+		 *  	- 1 (indica que queda un resto)
+		 *  
+		 *  Para estos casos tenemos el operador % (modulo)
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		
+		
+		
+		
+	}
+
+}
---
+++ prueba/src/operadores/Logicos.java
@@ -0,0 +1,22 @@
+package operadores;
+
+public class Logicos {
+
+	public static void main(String[] args) {
+		int edad2 = 19;
+		String provincia = "Sevilla";
+		
+		
+		boolean beca = (edad2 >= 18) && (provincia == "Sevilla"); 
+		
+		boolean llueve = true;
+		boolean tareas = true;
+		boolean biblio = true;
+		
+		boolean salirALaCalle = (llueve==false && tareas == true) || biblio == true;
+		
+		boolean salirALaCalle2 = (!llueve && tareas) || biblio;
+
+	}
+
+}
