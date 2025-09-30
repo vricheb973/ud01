9f5ff88afcd92af82fa84082c92875b775b411dd
vricheb973 <vricheb973@g.educaand.es>
2025-09-30 10:25:58 +0200

    Create Diapositiva64.java

---
+++ prueba/src/ejercicios/Diapositiva64.java
@@ -0,0 +1,18 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Diapositiva64 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("¿Cuántos años tienes?");
+		int edad = sc.nextInt();
+		
+		System.out.println("El año que viene tendrás " + ++edad + " años");		
+		
+	}
+
+}
